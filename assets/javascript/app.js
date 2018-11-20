6241cf76e9bdca89c3b90af3b3621691bf79d252
Brayden Copley <braydenc303@gmail.com>
2018-11-19 20:33:51 -0700

    Created the initial array for buttons/searches and finished the file structure.

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+var junkFood = ["cupcake", "burger", "hot dog", "chips", 
+                "candy bar", "lollipop", "popsicle", "pizza",
+                "buffalo wings", "fried chicken", "ice cream",
+                "pretzels", "cookies", "corndog", "nachos",
+                "chicken nuggets", "brownies", "candy", "popcorn",
+                "mac and cheese"]
