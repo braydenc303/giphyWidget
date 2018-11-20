e8f0481a3b936497f80ed05a28613cf5394b2b02
Brayden Copley <braydenc303@gmail.com>
2018-11-19 22:32:45 -0700

    .onclick is generating usable queryURLs. Next step is to actually generate an API call from that.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,3 +12,11 @@ function makeButtons() {
         $("header").append("<button data-category = '" + junkFood[i] + "'>" + junkFood[i].toUpperCase() + "</button>");
     }
 }
+
+$("header").on("click", "button", function() {
+    var category = $(this).attr("data-category");
+    console.log(category);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + category + "&api_key=STrsqiGUC2AsGFAUFLvmOUAMxcdUUt6a&limit=10";
+    console.log(queryURL);
+});
+
